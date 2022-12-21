92e4b327a373a1886e6bacb06342f798669b8e33
HeeGyeong01 <yhg9801@gmail.com>
2022-12-21 10:34:34 +0900

    응답으로 사분면 정보 추가

--- src/views/content_input_views.py
+++ src/views/content_input_views.py
@@ -90,6 +90,7 @@ def recommended_design():
     design_result["body_font"] = body_font
     design_result["color"] = color_result
     design_result["adj"] = adjective
+    design_result["quadrant"] = quadrant
     return json.dumps(design_result)
     # return "title font, body font와 rgb color list[126, 51, 203] * 3"
 
